1bbf13b7f8f8b85e66463d67657366aee160e792
surgeoftheages <surendhar.s@aurosociety.org>
2025-09-18 17:59:44 +0530

    Update docusaurus.config.js
    final check for live site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'aurobharati', // Usually your GitHub org/user name.
-  projectName: 'aurobharati.github.io', // Usually your repo name.
+  projectName: 'aurobharati.github.io', // your repo name.
   deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
