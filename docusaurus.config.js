3c8cd7b57f7a023c865d6239699f6f7acd972654
surgeoftheages <102948524+surgeoftheages@users.noreply.github.com>
2025-10-16 17:21:21 +0530

    adding a sample tagline
    sample tagline is here

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Siddhars Of Pondicherry',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Siddhars website',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
