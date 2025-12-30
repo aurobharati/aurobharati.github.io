d8c05fbb32774f5fb35cc58b53e2b431ec6be11a
Sure <surendhar.s@aurosociety.org>
2025-12-30 12:18:20 +0530

    final retouche changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/category/siddhars-of-pondicherry">
             Learn more about Siddhars </Link>
         </div>
       </div>
